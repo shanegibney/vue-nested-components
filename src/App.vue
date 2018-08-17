f021c804abdf942d51dcd935676aed86495428cd
shanegibney <shanegibney@gmail.com>
2018-08-17 11:00:22 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>I am parent component!</h1>
+    <Nested/>
+  </div>
+</template>
+
+<script>
+import Nested from './components/Nested'
+export default {
+  name: 'App',
+  components: {
+    Nested
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Nested.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <h1>I am a nested component</h1>
+</template>
+
+<script>
+export default {
+  name: 'Nested',
+  data(){
+    return
+  }
+}
+</script>
+
+<style lang="css">
+</style>
