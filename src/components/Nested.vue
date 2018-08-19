c2b1a98f934aecec3e93ef13841debe934382b78
shanegibney <shanegibney@gmail.com>
2018-08-19 19:00:01 +0100

    added nested nested component

--- src/components/Nested.vue
+++ src/components/Nested.vue
@@ -2,12 +2,17 @@
   <div>
     <h1>I am a nested component</h1>
     <a href="https://github.com/shanegibney/vue-nested-components">Return to repo</a>
+    <NestedNested/>
   </div>
 </template>
 
 <script>
+import NestedNested from './NestedNested'
 export default {
   name: 'Nested',
+  components: {
+    NestedNested
+  },
   data(){
     return
   }
---
+++ src/components/NestedNested.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+<div class="nestednested">
+  <h1>I am a nested-nested component</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NestedNested',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="css">
+</style>
